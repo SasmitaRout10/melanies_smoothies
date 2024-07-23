d08fb30b790761d1c82ed8219c1fe859bb26bce8
SasmitaRout10 <sasmitarout@gmail.com>
2024-07-23 16:18:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,15 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-  st.write(ingredients_list)
-  st.text(ingredients_list)
-    
-ingredients_string = ''
+  #st.write(ingredients_list)
+  #st.text(ingredients_list)
+    ingredients_string = ''
 
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ''
 
-  search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+  #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     st.subheader(fruit_chosen + ' Nutrition information')
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
