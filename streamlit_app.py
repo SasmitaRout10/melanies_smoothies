2867db3871149eb7f9ec2d4ada610995a52f108d
SasmitaRout10 <sasmitarout@gmail.com>
2024-07-23 17:08:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ if time_to_insert:
 
 st.success('Your Smoothie is ordered! ', icon="✅")
 
+#New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
