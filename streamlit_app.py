49521e013e21b62adf795886e289b8747cf73dd1
SasmitaRout10 <sasmitarout@gmail.com>
2024-07-23 17:54:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,28 +18,21 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
-#convert the snowpark dataframe to pandas dataframe so we can use LOC function
-#pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
-
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:'
 , my_dataframe
 , max_selections=5
 )
 
-if ingredients_list:   
-  ingredients_string = ''
+if ingredients_list:
+ st.write(ingredients_list)
+ st.text(ingredients_list)
+    
+ingredients_string = ''
 
 for fruit_choosen in ingredients_list:
-    ingredients_string += fruit_choosen + ''
- 
-    #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-    #st.subheader(fruit_choosen + ' Nutrition Information')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+    ingredients_string += fruit_choosen
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
 st.write(ingredients_string)
@@ -56,7 +49,3 @@ if time_to_insert:
     #session.sql(my_insert_stmt).collect(
 
 st.success('Your Smoothie is ordered! ', icon="✅")
-
-
-
-
